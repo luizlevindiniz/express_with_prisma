ffa3f5cbf4ba295f48b31557cbeb10de0ef95633
Luiz Diniz <luizd97@gmail.com>
2023-08-05 15:22:37 -0300

    updated db and env environment

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,22 @@
-import express, { Request, Response, NextFunction } from 'express'
-import { noteRouter } from './notes'
-import cors from 'cors'
-
+import express, { Request, Response, NextFunction } from "express";
+import { noteRouter } from "./notes";
+import cors from "cors";
 
 export function createApp() {
-    const app = express()
+  const app = express();
 
-    app.use(express.json())
+  app.use(express.json());
 
-    app.use(cors())
+  app.use(cors()); /*  prevent cors */
 
-    app.get('/health', (req, res) => res.status(200).send('OK'))
+  app.get("/health", (req, res) => res.status(200).send("OK"));
 
-    app.use(noteRouter)
+  app.use(noteRouter);
 
-    app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
-        console.error(err.stack)
-        res.status(500).send('Something broke!')
-    })
+  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+  });
 
-    return app
+  return app;
 }
